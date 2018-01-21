a1dd28f940622a95361693e7afb4e19ceab7dec4
Zerthick <zerthick@gmail.com>
2018-01-21 13:18:41 -0500

    Basic project structure.

---
+++ src/main/java/io/github/zerthick/mcskills/McSkills.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (C) 2018  Zerthick
+ *
+ * This file is part of mcSkills.
+ *
+ * mcSkills is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * mcSkills is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with mcSkills.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package io.github.zerthick.mcskills;
+
+import com.google.inject.Inject;
+import org.slf4j.Logger;
+import org.spongepowered.api.entity.living.player.Player;
+import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.filter.Getter;
+import org.spongepowered.api.event.game.state.GameStartedServerEvent;
+import org.spongepowered.api.event.network.ClientConnectionEvent;
+import org.spongepowered.api.plugin.Plugin;
+import org.spongepowered.api.text.Text;
+import org.spongepowered.api.text.format.TextColors;
+import org.spongepowered.api.text.format.TextStyles;
+
+@Plugin(
+        id = "mcskills",
+        name = "McSkills",
+        description = "A Minecraft RPG plugin",
+        authors = {
+                "Zerthick",
+                "Brian_Elliott"
+        }
+)
+public class McSkills {
+
+    @Inject
+    private Logger logger;
+
+    @Listener
+    public void onServerStart(GameStartedServerEvent event) {
+        // Hey! The server has started!
+        this.logger.info("Hello world!");
+        // Try loading some configuration settings for a welcome message to players
+        // when they join!
+    }
+
+
+    @Listener
+    public void onPlayerJoin(ClientConnectionEvent.Join event, @Getter("getTargetEntity") Player player) {
+        // The text message could be configurable, check the docs on how to do so!
+        player.sendMessage(Text.of(TextColors.AQUA, TextStyles.BOLD, "Hi " + player.getName()));
+    }
+
+}
